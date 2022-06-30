01c10e0a053abd7c4352e94aea3064fc717da5a2
Виктор Редькин <Moridin830@gmail.com>
2022-06-30 23:55:55 +0300

    Добавлен просмотр логов.

---
+++ Task2/Controllers/LogsController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Task2.Models;
+using Task2.Models.Db;
+using Task2.Repositories;
+using Task2.Repositories.LoggerRepository;
+
+namespace Task2.Controllers
+{
+    public class LogsController : Controller
+    {
+        private ILoggingRepository _loggerRepository;
+
+        public LogsController(ILoggingRepository loggerRepository)
+        {
+            _loggerRepository = loggerRepository;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var requests = await _loggerRepository.GetRequests();
+            return View(requests);
+        }
+    }
+}
